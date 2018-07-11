f1b5cf3ba7e6f24fad88dc0baca66134d7e822a2
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-11 14:25:24 +0200

    34

--- cron.sh
+++ cron.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 authorization = "ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg=="
-ticket = "ONENETWORK-2141"
+ticket = "ONENETWORK-2346"
 var1 = $var1
 log = git log --pretty=format: "%s"
 
 #https://jira.atlassian.com/rest/api/latest/issue/JRA-9
 #https://pid.hardis.fr/jira/browse/
-curl -D- -X GET -H "Authorization: Basic " "$authorization" -H "Content-Type: application/json" "https://pid.hardis.fr/rest/api/latest/issue/" "${ticket}"
+curl -D- -X GET -H "Authorization: Basic " "$authorization" -H "Content-Type: application/json" "https://pid.hardis.fr/rest/api/latest/browse/" "${ticket}"
 
 echo $log "test"
 git log --pretty=format:"%s" -1
