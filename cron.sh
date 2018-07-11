0613f6267abef53645c67d4b8afc891f07e9ddfa
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-11 11:53:21 +0200

    test

--- cron.sh
+++ cron.sh
@@ -8,3 +8,5 @@ log = git log -1
 curl -D- -X GET -H "Authorization: Basic " "$authorization" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/browse/" "$ticket"
 
 echo $log "test"
+
+exit
