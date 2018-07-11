d297da3d1e0e7d53216c5a9332075276960cd1c9
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-11 11:39:20 +0200

    test

--- cron.sh
+++ cron.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-authorization = 'ddaunique:D17051996oreeN*' | base64
+authorization = 'ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg=='
 ticket = 'ONENETWORK-2141'
 log = git log -1
 
 curl -D- -X GET -H "Authorization: Basic " "$authorization" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/browse/" "$ticket"
 
-echo $log
+echo $log "test"
