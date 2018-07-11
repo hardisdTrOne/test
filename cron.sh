13ae874c82d3f2ecb368f95c0191a626b7f38822
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-11 11:42:40 +0200

    test

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-authorization = 'ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg=='
-ticket = 'ONENETWORK-2141'
+authorization = ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==
+ticket = ONENETWORK-2141
 log = git log -1
 
 curl -D- -X GET -H "Authorization: Basic " "$authorization" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/browse/" "$ticket"
