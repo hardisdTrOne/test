1ea89a45b253fdad184c89ccb529fdd44d2c308f
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-12 10:58:27 +0200

    commentaire5

--- cron.sh
+++ cron.sh
@@ -6,6 +6,7 @@ var1 = $1
 #https://pid.hardis.fr/jira/browse/
 curl -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/ONENETWORK-2346"
 
+echo " "
 echo $var1 "fin"
 git log --pretty=format:"%s" -1
 
