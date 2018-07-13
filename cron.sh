59c3d3990d08db022728567d2f36f89433b0e64e
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-13 18:11:30 +0200

    ONENETET-1611

--- cron.sh
+++ cron.sh
@@ -1,36 +1,14 @@
 #!/usr/bin/env bash
 
 
-
 resultcurl=$(curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1" ">api.json")
 
-
-
-echo " "
-echo "--------------------------------"
-
-
-
-
 result= "$resultcurl" | grep -Po '"labels":\[(.*")\]' api.json | awk -F ":" '{print $2}' | awk -F "\"" '{print $2}'
-
-
-echo "///////////////////////////////////////////////"
-
 
 echo "$result"
 var1="ok"
 
 export result
 ./.git/hooks/commit-msg $result $var1
-
-echo " "
-echo "--------------------------------"
-
-
-
-
-echo " "
-echo "$1" "fin"
 
 exit
