b99bd34b71ea2c12aec4804b8f7433f290ba8a0a
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-13 17:02:27 +0200

    ONENET-1611

--- cron.php
+++ cron.php
@@ -6,8 +6,8 @@
  * Time: 15:58
  */
 
-
-$issus= $test;
+var_dump($test);
+$issus= "";
 exec('curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/'.$issus.'" >api.json', $output);
 
 print_r($output);
