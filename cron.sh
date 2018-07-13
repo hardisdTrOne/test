2447405501e4e8dc13b88dbee2bfb823ad9b67c0
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-13 18:07:15 +0200

    ONENETET-1611

--- cron.sh
+++ cron.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
 
-#https://jira.atlassian.com/rest/api/latest/issue/JRA-9
-#https://pid.hardis.fr/jira/browse/
-#https://pid.hardis.fr/jira/rest/api/latest/issue/ONENETWORK-2346
-
-#echo "https://pid.hardis.fr/jira/rest/api/latest/issue/$1/?fields=labels"
 
 resultcurl=$(curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1" ">api.json")
 
-#curl -s -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1"
 
 
 echo " "
 echo "--------------------------------"
 
-
-#branch=$(grep "labels" )
 
 
 
