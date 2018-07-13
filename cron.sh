bb0eba6e5691bc1a28e610bea53d972b5b0bbb2c
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-13 11:26:45 +0200

    ONENETWORK-2346

--- cron.sh
+++ cron.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-var1 = $1
 
 #https://jira.atlassian.com/rest/api/latest/issue/JRA-9
 #https://pid.hardis.fr/jira/browse/
-curl -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/ONENETWORK-2346"
+#https://pid.hardis.fr/jira/rest/api/latest/issue/ONENETWORK-2346
+curl -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/""$1"
 
 echo " "
-echo $var1 "fin"
-git log --pretty=format:"%s" -1
+echo "$var1" "fin"
 
 exit
