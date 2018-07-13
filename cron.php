2bd5fd714edf14f59097deb19597f7d3bf45b2c0
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-13 15:38:34 +0200

    ONENET-1611

---
+++ cron.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doreen
+ * Date: 13/07/18
+ * Time: 15:36
+ */
--- cron.sh
+++ cron.sh
@@ -4,12 +4,37 @@
 #https://jira.atlassian.com/rest/api/latest/issue/JRA-9
 #https://pid.hardis.fr/jira/browse/
 #https://pid.hardis.fr/jira/rest/api/latest/issue/ONENETWORK-2346
-echo $1 ;
-echo "https://pid.hardis.fr/jira/rest/api/latest/issue/$1" "$1"
 
-curl -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/""$1"
+echo "https://pid.hardis.fr/jira/rest/api/latest/issue/$1/?fields=labels"
+
+resultcurl=$(curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1")
+
+#curl -s -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1"
+
 
 echo " "
-echo "$var1" "fin"
+echo "--------------------------------"
+
+
+#branch=$(grep "labels" )
+
+
+
+result= "$resultcurl" | grep '"labels":\[(.*")\]'
+
+echo "///////////////////////////////////////////////"
+
+
+echo "$result"
+
+
+echo " "
+echo "--------------------------------"
+
+
+
+
+echo " "
+echo "$1" "fin"
 
 exit
