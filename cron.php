dbe577e089592033c3fef316786cc0122baa6bcd
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-13 16:02:20 +0200

    ONENET-1611

--- cron.php
+++ cron.php
@@ -5,3 +5,7 @@
  * Date: 13/07/18
  * Time: 15:58
  */
+
+exec('curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1"', $output);
+
+print_r($output);
