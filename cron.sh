1f842162418806833425363643f57d63c8866c00
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-16 14:45:24 +0200

    ONENET-1611

--- cron.sh
+++ cron.sh
@@ -4,11 +4,10 @@
 
 resultcurl=$(curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1")
 
-echo "$resultcurl" > /tmp/api.json
+echo "$resultcurl" > /tmp/api.json | less
 
 branche= grep -Po '"labels":\[(.*")\]' api.json | awk -F ":" '{print $2}' | awk -F "\"" '{print $2}'
 
 echo $branche
-
 
 exit 0;
