b23efb0910ecd62b57f8ea4ccc82adcac203adb4
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-16 16:01:01 +0200

    ONENETWORK-2291

--- cron.sh
+++ cron.sh
@@ -8,7 +8,7 @@ branche= grep -Po '"labels":\[(.*")\]' ./tmp/api.json | awk -F ":" '{print $2}' 
 
 status= grep -Po '"labels":\[(.*")\]' ./tmp/api.json |  awk -F "\"description\":\"La demande est " '{print $2}' | awk -F "\"" '{print $1}'
 
-echo $branche="$branche"
+echo $branche
 
 echo "$status"
 
