e0a2c8ebb7b49574824c6f6092e2817dcdb75819
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-16 16:47:31 +0200

    ONENETWORK-1416

--- cron.sh
+++ cron.sh
@@ -8,4 +8,6 @@ branche= grep -Po '"labels":\[(.*")\]' ./tmp/api.json | awk -F ":" '{print $2}' 
 
 ticket_status= grep -Po '"labels":\[(.*")\]' ./tmp/api.json |  awk -F "\"description\":\"La demande est " '{print $2}' | awk -F "\"" '{print $1}'
 
+return $branche, $tiket_status
+
 exit 0;
