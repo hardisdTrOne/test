f515be0bfe62de8466cf1786fef0f05715a1c5a3
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-16 12:14:21 +0200

    ONENET-1611

--- cron.sh
+++ cron.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm /tmp/api.json
+
 resultcurl=$(curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1")
 
 echo "$resultcurl" > /tmp/api.json
