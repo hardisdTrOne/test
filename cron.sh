39fcb9ae831da01261bab361004c0326c68f8c1b
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-17 10:11:24 +0200

    ONENETWORK-1416

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-resultcurl=$(curl -s -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1")
+resultcurl=$(curl -D- -X GET -H "Authorization: Basic ZGRhdW5pcXVlOkQxNzA1MTk5Nm9yZWVOKg==" -H "Content-Type: application/json" "https://pid.hardis.fr/jira/rest/api/latest/issue/$1")
 
 echo "$resultcurl">./tmp/api.json
 
