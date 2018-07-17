e3e94464250db2535936d8e7a0bfe606ee680fc5
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-17 17:19:42 +0200

    ONENETWORK-141515151

--- cron.sh
+++ cron.sh
@@ -6,23 +6,25 @@ echo "$resultcurl">./tmp/api.json
 
 ticket=$(grep -Po '"errorMessages":\[(.*")\]' ./tmp/api.json | awk -F ":" '{print $2}' | awk -F "\"" '{print $2}'| awk '{print toupper($0)}')
 
-
+if [ -z "$ticket" ] ;
+then
+echo "ERREUR.( ""$ticket"" )" >&2
+exit 1;
+else
 ticket_status=$(grep -Po '"status":(.*)}},(.*)"components":' ./tmp/api.json | awk -F "\"name\":" '{print $2}'| awk -F "\"" '{print $2}'| awk '{print toupper($0)}')
+if [ "$ticket_status" != "EN COURS" ] ;then
+echo "Le status n'est pas en cours.( ""$ticket_status"" )" >&2
+exit 1;
+fi
+fi
 
 branche= grep -Po '"labels":\[(.*")\]' ./tmp/api.json | awk -F ":" '{print $2}' | awk -F "\"" '{print $2}'| awk '{print toupper($0)}'
 
 
-if [ -z "$ticket" ] ;then
-echo "ERREUR.( ""$ticket"" )" >&2
-exit 1;
-fi
+
 
 if [ -z "$branche" ] ;then
 echo "La branche du ticket n'est pas renseignée.( ""$branche"" )" >&2
 exit 1;
 fi
 
-if [ "$ticket_status" != "EN COURS" ] ;then
-echo "Le status n'est pas en cours.( ""$ticket_status"" )" >&2
-exit 1;
-fi
