cbbbfc4706473292cfa48ca38156dac474466925
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-17 17:10:50 +0200

    ONENETWORK-1416

--- cron.sh
+++ cron.sh
@@ -6,9 +6,11 @@ echo "$resultcurl">./tmp/api.json
 
 branche= grep -Po '"labels":\[(.*")\]' ./tmp/api.json | awk -F ":" '{print $2}' | awk -F "\"" '{print $2}'| awk '{print toupper($0)}'
 
-printf "$branche"
 
-
+if [ -z "$resultcurl" ] ;then
+echo "La branche du ticket n'est pas renseignée.( ""$branche"" )" >&2
+exit 1;
+fi
 
 
 ticket_status=$(grep -Po '"status":(.*)}},(.*)"components":' ./tmp/api.json | awk -F "\"name\":" '{print $2}'| awk -F "\"" '{print $2}'| awk '{print toupper($0)}')
