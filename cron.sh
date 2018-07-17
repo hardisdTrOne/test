91dd51562355991338ed1a5fe68b38c399f13b80
DoreenDaunique <daunique.doreen@gmail.com>
2018-07-17 11:36:37 +0200

    ONENETWORK-1416

--- cron.sh
+++ cron.sh
@@ -7,7 +7,6 @@ echo "$resultcurl">./tmp/api.json
 branche= grep -Po '"labels":\[(.*")\]' ./tmp/api.json | awk -F ":" '{print $2}' | awk -F "\"" '{print $2}'
 
 echo "$resultcurl">./tmp/api.json
-#ticket_status= grep -Po '"labels":\[(.*")\]' ./tmp/api.json |  awk -F "\"description\":\"La demande est " '{print $2}' | awk -F "\"" '{print $1}'
-ticket_status= grep -Po '"status":(.*)}},(.*)"components":' ./tmp/api.json | awk -F "\"name\":" '{print $2}'| awk -F "\"" '{print $2}'
+ticket_status= grep -Po '"status":(.*)}},(.*)"components":' ./tmp/api.json | awk -F "\"name\":" '{print $2}'| awk -F "\"" '{print $2}'| awk '{print toupper($0)}'
 
 exit 0;
